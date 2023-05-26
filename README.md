4ed91c0914457442ab45adefa797f7912ea18db6
MikeZuck1 <105445747+MikeZuck1@users.noreply.github.com>
2023-05-26 23:47:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-chatbot
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-iwjatx)
---
+++ index.js
@@ -0,0 +1,64 @@
+// Import stylesheets
+import './style.css';
+
+const chatbox = document.getElementById('chatbox'); 
+    const tabs = document.getElementsByClassName('tab'); 
+    const apiKey = 'duffel_live_8c7IGwY5zLynTKISd2oLFLMRBw6Utce1u_kMpbccRRy';  
+
+    for (let i = 0; i < tabs.length; i++) {
+      tabs[i].addEventListener('click', function() {  
+        const tabText = this.textContent;
+        let message = '';  
+
+        if (tabText === 'Vols') { 
+          message = "Quelle est votre ville de départ ?";
+          displayMessage(message);  
+          displayDestinationCities(); 
+        } else if (tabText === 'Hôtels') {  
+          // Code to retrieve hotel-related message using Duffel API
+          // Replace the following line with the appropriate API call
+          message = "Quel est votre hôtel ?";  
+          displayMessage(message); 
+        } else if (tabText === 'Villas') { 
+          // Code to retrieve villa-related message using Duffel API
+          // Replace the following line with the appropriate API call
+          message = "Quel villa choisissez-vous ?";
+          displayMessage(message);
+        } else if (tabText === 'Appartement') {
+          // Code to retrieve apartment-related message using Duffel API
+          // Replace the following line with the appropriate API call
+          message = "Quel type appartement choisissez-vous ?";
+          displayMessage(message);  
+        }
+      });
+    }
+
+    function displayMessage(message) { 
+      const messageElement = document.createElement('p');
+      messageElement.textContent = message;
+      chatbox.appendChild(messageElement);
+    }
+
+    function displayDestinationCities() { 
+      const cities = ["Paris", "Barcelone", "Dubaï", "New York", "Londres", "Tokyo", "Rome"];
+      const destinationMessage = "Voici quelques villes de destination : "; 
+
+      for (let i = 0; i < cities.length; i++) {
+        destinationMessage += cities[i]; 
+
+        if (i !== cities.length - 1) {
+          destinationMessage += ", "; 
+        }
+      }
+
+      displayMessage(destinationMessage);
+    }
+
+    displayMessage("Bonjour, je suis un assistant virtuel. Je suis là pour vous aider à planifier parfaitement votre prochain voyage ! Chattez avec moi ou cliquez sur l'une des options ci-dessous :");
+  
+
+
+
+
+
+
